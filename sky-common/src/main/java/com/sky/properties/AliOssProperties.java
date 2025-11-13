b35ee087d5c96dd27b6cd6c28a3b215aff2d0401
ascendatucla@gmail.com <83774460+lailaidai@users.noreply.github.com>
2025-11-12 21:45:33 -0600

    新增员工业务代码开发

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -14,4 +14,40 @@ public class AliOssProperties {
     private String accessKeySecret;
     private String bucketName;
 
+    //getter setter
+    public String getEndpoint() {
+        return endpoint;
+    }
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+    public String getAccessKeyId() {
+        return accessKeyId;
+    }
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+    public String getAccessKeySecret() {
+        return accessKeySecret;
+    }
+    public void setAccessKeySecret(String accessKeySecret) {
+        this.accessKeySecret = accessKeySecret;
+    }
+    public String getBucketName() {
+        return bucketName;
+    }
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    //tostring
+    @Override
+    public String toString() {
+        return "AliOssProperties{" +
+                "endpoint='" + endpoint + '\'' +
+                ", accessKeyId='" + accessKeyId + '\'' +
+                ", accessKeySecret='" + accessKeySecret + '\'' +
+                ", bucketName='" + bucketName + '\'' +
+                '}';
+    }
 }
--- sky-common/src/main/java/com/sky/utils/WeChatPayUtil.java
+++ sky-common/src/main/java/com/sky/utils/WeChatPayUtil.java
@@ -29,6 +29,7 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
+
 /**
  * 微信支付工具类
  */
